4a3ac5d4415427f08dbdf1b2bd713435b46506d1
Esteban Frare <frare.esteban@gmail.com>
2019-08-05 16:52:34 -0300

    HCS: add AuthRequired on Home route

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { testAction } from '../redux/actions'
+import AuthRequired from './auth-required'
 import Login from './login'
 import Home from './home'
 import './styles.css'
@@ -10,7 +11,14 @@ const App = (props) => (
   <BrowserRouter>
     <div className="app-container">
       <Route path="/" exact={true} component={Login} />
-      <Route path="/home" exact={true} component={Home} />
+      <Route
+        exact path="/home"
+        render={() => (
+          <AuthRequired
+            redirectTo='/'
+            orRender={<Home />}
+          />)}
+      />
     </div>
   </BrowserRouter>
 )
