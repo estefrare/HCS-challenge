96c276bc2313fe8d781202d991591704c003ed2a
Esteban Frare <frare.esteban@gmail.com>
2019-08-05 16:05:19 -0300

    HCS: add react-router

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { testAction } from '../redux/actions'
 import Login from './login'
 import './styles.css'
 
 const App = (props) => (
-  <div className="app-container">
-    <Login />
-  </div>
+  <BrowserRouter>
+    <div className="app-container">
+      <Route path="/" exact={true} component={Login} />
+    </div>
+  </BrowserRouter>
 )
 
 export default App
